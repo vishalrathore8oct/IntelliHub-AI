d2f59f018735eb0dc1db64df21a4cc720529ca12
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-10-23 12:41:05 +0530

    add the login, signup button funcnality using clerk service with google and email authentication with profile settings

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { use } from "react";
 import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { ArrowRight } from "lucide-react";
+import { useClerk, useUser, UserButton } from "@clerk/clerk-react";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
 
   return (
     <div className="fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32">
@@ -14,9 +17,16 @@ const Navbar = () => {
         alt="logo"
         onClick={() => navigate("/")}
       />
-      <button className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5">
-        Get Started <ArrowRight className="w-4 h-4" />
-      </button>
+      {user ? (
+        <UserButton />
+      ) : (
+        <button
+          onClick={openSignIn}
+          className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
+        >
+          Get Started <ArrowRight className="w-4 h-4" />
+        </button>
+      )}
     </div>
   );
 };
