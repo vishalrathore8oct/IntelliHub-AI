f5f37f99a6a82a728b28140bf3d1560bc0be0547
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-10-23 12:00:16 +0530

    add the routes and rested routing with outlet layout for all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import WriteArticle from "./pages/WriteArticle";
+import BlogTitles from "./pages/BlogTitles";
+import GenerateImages from "./pages/GenerateImages";
+import RemoveBackground from "./pages/RemoveBackground";
+import RemoveObject from "./pages/RemoveObject";
+import ReviewResume from "./pages/ReviewResume";
+import Community from "./pages/Community";
 
 const App = () => {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="ai" element={<Layout />}>
+          <Route index element={<Dashboard />}/>
+          <Route path="write-article" element={<WriteArticle />} />
+          <Route path="blog-titles" element={<BlogTitles />} />
+          <Route path="generate-images" element={<GenerateImages />} />
+          <Route path="remove-background" element={<RemoveBackground />} />
+          <Route path="remove-object" element={<RemoveObject />} />
+          <Route path="review-resume" element={<ReviewResume />} />
+          <Route path="'communitye" element={<Community />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div>Layout</div>
-  )
-}
+    <>
+      <div>Layout</div>
+      <Outlet />
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
