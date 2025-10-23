cd6531ff0ee002e7a69e052b0aff750e73e0255c
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-10-23 15:07:46 +0530

    create the ui of footer  section of home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Hero from "../components/Hero";
 import AiTools from "../components/AiTools";
 import Testimonial from "../components/Testimonial";
 import Subscription from "../components/Subscription";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <AiTools/>
       <Testimonial/>
       <Subscription />
+      <Footer />
     </>
   );
 };
