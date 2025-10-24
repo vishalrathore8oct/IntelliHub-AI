3ce09517a426915c823457a6d1451dc04bf8a232
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-10-24 11:57:13 +0530

    install the react-markdown and fix the billing sidebar ui issue and also complete the Dashboard component with mock data with a newly created creationItem component

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { dummyCreationData } from "../assets/assets";
+import { Gem, Sparkle } from "lucide-react";
+import { Protect } from "@clerk/clerk-react";
+import CreationItems from "../components/CreationItems";
+
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [creations, setCreations] = useState([]);
+  const getDashboardData = async () => {
+    setCreations(dummyCreationData);
+  };
+  console.log(creations);
 
-export default Dashboard
+  useEffect(() => {
+    getDashboardData();
+  }, []);
+
+  return (
+    <div className="h-full overflow-y-scroll p-6">
+      <div className="flex justify-start gap-4 flex-wrap">
+        <div className="flex items-center justify-between w-72 p-4 px-6 bg-white rounded-b-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p className="text-sm">Total Creations</p>
+            <h2 className="text-xl font-semibold">{creations.length}</h2>
+          </div>
+          <div className="w-10 h-10 rounded-lg bg-linear-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center">
+            <Sparkle className="w-5 text-white" />
+          
+          </div>
+        </div>
+        <div className="flex items-center justify-between w-72 p-4 px-6 bg-white rounded-b-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p className="text-sm">Active Plan</p>
+            <h2 className="text-xl font-semibold">
+              <Protect plan="premium" fallback="Free">
+                Premium
+              </Protect>
+            </h2>
+          </div>
+          <div className="w-10 h-10 rounded-lg bg-linear-to-br from-[#FF61C5] to-[#9E53EE] text-white flex justify-center items-center">
+            <Gem className="w-5 text-white" />
+          
+          </div>
+        </div>
+      </div>
+      <div className="space-y-3">
+        <p className="mt-6 mb-4">Recent Creations</p>
+        {creations.map((item) => (
+          <CreationItems key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  )
+};
+
+export default Dashboard;
