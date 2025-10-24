8918c46a63793f717af6d5ff9e357041ae42afec
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-10-24 20:28:23 +0530

    create the neon sql database in config folder, install and setup clerk auth middleware in express server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
 import express from "express"
 import cors from "cors"
 import "dotenv/config"
+import { clerkMiddleware, requireAuth } from '@clerk/express'
+
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(clerkMiddleware())
+
 
 app.get("/", (req, res) => {
     res.send("IntelliHub-AI Server is Running!")
 })
+
+app.use(requireAuth())
 
 const PORT = process.env.PORT || 8080
 
