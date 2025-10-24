b14c652e60d70c06b8d72b1037d5f64939256390
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-10-24 12:54:55 +0530

    complete the blog-titles component ui without api integration

--- client/src/pages/BlogTitles.jsx
+++ client/src/pages/BlogTitles.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Edit, Hash, Sparkle, Sparkles } from "lucide-react";
 
 const BlogTitles = () => {
-  return (
-    <div>BlogTitles</div>
-  )
-}
+  const blogCategories = [
+    "General",
+    "Technology",
+    "Business",
+    "Health",
+    "Lifestyle",
+    "Education",
+    "Travel",
+    "Food",
+  ];
 
-export default BlogTitles
+  const [selectedCategory, setSelectedCategory] = useState(blogCategories[0]);
+  const [input, setInput] = useState("");
+  const submitHandler = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="h-full overflow-y-scroll p-6 flex flex-wrap items-start gap-4 text-slate-700">
+      <form
+        onSubmit={submitHandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#8E37EB]" />
+          <h1 className="text-xl font-semibold">AI Title Generator</h1>
+        </div>
+        <p className="mt-6 text-sm font-medium">Keyword</p>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+          type="text"
+          required
+          placeholder="The Future of Artificial Intelligence is...."
+        />
+        <p className="mt-4 text-sm font-medium">Category</p>
+        <div className="mt-3 flex gap-3 flex-wrap sm:max-w-9/11">
+          {blogCategories.map((item, index) => (
+            <span
+              onClick={() => setSelectedCategory(item)}
+              className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${
+                selectedCategory === item
+                  ? "bg-purple-50 text-purple-700"
+                  : "text-gray-500 border-gray-300"
+              }`}
+              key={index}
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+        <br />
+        <button
+          type="submit"
+          className="w-full flex justify-center items-center gap-2 bg-linear-to-r from-[#C341F6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer"
+        >
+          {" "}
+          <Hash className="w-5" /> Generate Title
+        </button>
+      </form>
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-88">
+        <div className="flex items-center gap-3">
+          <Hash className="w-5 h-5 text-[#8E37EB]" />
+
+          <h1 className="text-xl font-semibold">Generated Titles</h1>
+        </div>
+
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <Hash className="w-9 h-9" />
+
+            <p>Enter a topic and click “Generate Titles” to get started</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogTitles;
--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -58,7 +58,7 @@ const WriteArticle = () => {
           <Edit className="w-5" /> Generate Article
         </button>
       </form>
-      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-88 max-h-[600px]">
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-88">
         <div className="flex items-center gap-3">
           <Edit className="w-5 h-5 text-[#4A7AFF]" />
 
@@ -69,7 +69,7 @@ const WriteArticle = () => {
           <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
             <Edit className="w-9 h-9" />
 
-            <p>Enter a topic and click “Generate article ” to get started</p>
+            <p>Enter a topic and click “Generate article” to get started</p>
           </div>
         </div>
       </div>
