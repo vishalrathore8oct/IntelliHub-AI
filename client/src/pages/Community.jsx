2933ac5dde282d08919bc0aa5312d894f805dc06
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-10-24 15:13:35 +0530

    complete the community component ui without api integration

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useUser } from "@clerk/clerk-react";
+import { dummyPublishedCreationData } from "../assets/assets";
+import { Heart } from "lucide-react";
 
 const Community = () => {
-  return (
-    <div>Community</div>
-  )
-}
+  const [creations, setCreations] = useState([]);
+  const { user } = useUser();
+  const fetchCreations = async () => {
+    setCreations(dummyPublishedCreationData);
+  };
 
-export default Community
+  useEffect(() => {
+    fetchCreations();
+  }, []);
+
+  return (
+    <div className="flex-1 h-full flex flex-col gap-4 p-6">
+      Creations
+      <div className="bg-white h-full, w-full, rounded-xl overflow-x-scroll">
+        {creations.map((creation, index) => (
+          <div
+            key={index}
+            className="relative group inline-block pl-3 pt-3 w-full sm:max-w-1/2 lg:max-w-1/3"
+          >
+            <img
+              src={creation.content}
+              alt=""
+              className="w-full h-full object-cover rounded-lg"
+            />
+            <div className="absolute bottom-0 top-0 right-0 left-3 flex gap-2 items-end justify-end group-hover:justify-between p-3 group-hover:bg-linear-to-b from-transparent to-black/80 text-white rounded-lg">
+              <p className="text-sm hidden group-hover:block text-white mb-2">
+                {creation.prompt}
+              </p>
+              <div className="flex gap-1 items-center text-white">
+                <p>{creation.likes.length}</p>
+                <Heart
+                  className={`min-w-5 h-5 hover:scale-110 cursor-pointer ${
+                    creation.likes.includes(user.id)
+                      ? "fill-red-500 text-red-600"
+                      : "text-white"
+                  }`}
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Community;
