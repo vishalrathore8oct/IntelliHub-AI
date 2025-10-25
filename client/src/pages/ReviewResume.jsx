b8479c8bd6b83766d0bff8dccebc831403c5c593
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-10-25 14:00:00 +0530

    complete the api integration on review-resume section page

--- client/src/pages/ReviewResume.jsx
+++ client/src/pages/ReviewResume.jsx
@@ -1,11 +1,43 @@
 import { FileText, Sparkles } from "lucide-react";
 import { useState } from "react";
+import axios from "axios";
+import { useAuth } from "@clerk/clerk-react";
+import toast from "react-hot-toast";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const ReviewResume = () => {
   const [input, setInput] = useState("");
+
+  const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState("");
+  const { getToken } = useAuth();
+
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log("Input", input);
+    try {
+      setLoading(true);
+
+      const formData = new FormData();
+      formData.append("resume", input);
+
+      const { data } = await axios.post("/api/ai/resume-review", formData, {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+        },
+      });
+
+      if (data.success) {
+        setContent(data.content);
+        toast.success("Resume Reviewed successfully!");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
   return (
     <div className="h-full overflow-y-scroll p-6 flex flex-wrap items-start gap-4 text-slate-700">
@@ -29,11 +61,16 @@ const ReviewResume = () => {
           Supprots PDF Formats Resume Only.
         </p>
         <button
+          disabled={loading}
           type="submit"
           className="w-full flex justify-center items-center gap-2 bg-linear-to-r from-[#00DA83] to-[#009BB3] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer"
         >
-          {" "}
-          <FileText className="w-5" /> Review Resume
+          {loading ? (
+            <span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin"></span>
+          ) : (
+            <FileText className="w-5" />
+          )}{" "}
+          Review Resume
         </button>
       </form>
       <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-88">
@@ -43,13 +80,23 @@ const ReviewResume = () => {
           <h1 className="text-xl font-semibold">Analysis Results</h1>
         </div>
 
-        <div className="flex-1 flex justify-center items-center">
-          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
-            <FileText className="w-9 h-9" />
+        {!content ? (
+          <div className="flex-1 flex justify-center items-center">
+            <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+              <FileText className="w-9 h-9" />
 
-            <p>Upload a Resuem Pdf and click “Review Resume” to get started</p>
+              <p>
+                Upload a Resuem Pdf and click “Review Resume” to get started
+              </p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="mt-3 h-full overflow-y-scroll text-sm text-slate-600">
+            <div className="reset-tw">
+              <Markdown>{content}</Markdown>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
