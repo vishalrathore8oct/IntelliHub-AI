b0c255eefc0a98e1263b8f01f349010f913f5dda
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2025-10-25 11:36:55 +0530

    write the user controller of getUserCreations getPublishedCreations toggleLikeCreation

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,60 @@
+import sql from "../configs/db.js";
+
+export const getUserCreations = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const creations =
+      await sql`SELECT * FROM creations WHERE user_id = ${userId} ORDER BY created_at DESC`;
+    res.json({ success: true, creations });
+  } catch (error) {
+    console.log("error", error.massege);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getPublishedCreations = async (req, res) => {
+  try {
+    const creations =
+      await sql`SELECT * FROM creations WHERE publish = true ORDER BY created_at DESC`;
+    res.json({ success: true, creations });
+  } catch (error) {
+    console.log("error", error.massege);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const toggleLikeCreation = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { creationId } = req.body;
+
+    const [creation] =
+      await sql`SELECT * FROM creations WHERE id = ${creationId}`;
+
+    if (!creation) {
+      return res.json({ success: false, message: "Creation not found" });
+    }
+
+    const currentLikes = creation.likes;
+    const userIdStr = userId.toString();
+    let updatedLikes;
+    let message;
+
+    if (currentLikes.includes(userIdStr)) {
+      updatedLikes = currentLikes.filter((id) => id !== userIdStr);
+      message = "Creation Unliked";
+    } else {
+      updatedLikes = [...currentLikes, userIdStr];
+      message = "Creation Liked";
+    }
+
+    const formattedArray = `{${updatedLikes.join(",")}}`;
+
+    await sql`UPDATE creations SET likes = ${formattedArray}::text[] WHERE id = ${creationId}`;
+
+    res.json({ success: true, message });
+  } catch (error) {
+    console.log("error", error.massege);
+    res.json({ success: false, message: error.message });
+  }
+};
